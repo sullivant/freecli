c338ad273d1f7c9a59a1459aa491dafa5f0265f2
Thomas Sullivan <thomas.sullivan@protedyne.com>
2025-07-09 13:16:35 -0400

    migrates to a better dir structure.

--- src/io.rs
+++ src/io.rs
@@ -1,21 +1,23 @@
 use std::fs::{self, remove_file, File, OpenOptions};
 use std::io::{Read, Write, ErrorKind, self};
 use std::env;
-use std::path::PathBuf;
+use std::path::{MAIN_SEPARATOR, PathBuf};
 use crate::gamestate::GameState;
 use crate::stats::GameStats;
 use serde_json;
 
-static GAME_STATS_FILE: &str = ".freecli_stats.json";
-static GAME_STATE_FILE: &str = ".freecli_state.json";
+static GAME_ROOT: &str = ".freecli";
+static GAME_STATS_FILE: &str = "freecli_stats.json";
+static GAME_STATE_FILE: &str = "freecli_state.json";
+
 
 /// Returns the appropriate path to use when considering saves, loads, etc.
 /// 
 /// Will default to a user's home directory and if unable to will use the
 /// directory from which the application was executed.
-pub fn target_path() -> String {
+pub fn target_path(path: &str) -> String {
     let home_dir = dirs::home_dir();
-    let target_dir: PathBuf;
+    let mut target_dir: PathBuf;
 
     if let Some(home) = home_dir {
         if fs::metadata(&home).map(|m| m.permissions().readonly()).unwrap_or(true) {
@@ -27,19 +29,26 @@ pub fn target_path() -> String {
         target_dir = env::current_dir().expect("Failed to get current directory");
     }
 
+    // Push on our game's core root dir
+    target_dir.push(GAME_ROOT);
+
+    if !target_dir.exists() {
+        match fs::create_dir_all(&target_dir) {
+            Ok(_) => println!("Directory created: {}", target_dir.display()),
+            Err(e) => eprintln!("Failed to create directory: {}", e),
+        }
+    } 
+
     match target_dir.as_os_str().to_str() {
-        Some(s) => s.to_string(),
+        Some(s) => format!("{}{}{}", s.to_string(), MAIN_SEPARATOR, path),
         _ => "".to_string()
     }
 }
 
 pub fn load_stats() -> io::Result<GameStats> {
-    let target = target_path();
-    println!("Using target:{}",target);
+    let target = target_path(GAME_STATS_FILE);
 
-
-    // Loads .game_stats.json
-    match fs::read_to_string(GAME_STATS_FILE) {
+    match fs::read_to_string(target) {
         Ok(content) => Ok(serde_json::from_str(&content)?), // Load if ok to load...
         Err(e) if e.kind() == ErrorKind::NotFound => Ok(GameStats::default()), // Create new if not found.
         Err(e) => Err(e), // Bubble up any other error.  Permissions, etc.
@@ -48,24 +57,29 @@ pub fn load_stats() -> io::Result<GameStats> {
 
 pub fn save_stats(stats: &GameStats) -> io::Result<()> {
     let json = serde_json::to_string_pretty(stats)?;
-    fs::write(GAME_STATS_FILE, json)
+    let target = target_path(GAME_STATS_FILE);
+
+    fs::write(target, json)
 }
 
 pub fn save_game(game: &GameState) -> Result<(), Box<dyn std::error::Error>> {
     let json = serde_json::to_string_pretty(game)?;
 
+    let target = target_path(GAME_STATE_FILE);
+    
     let mut file = OpenOptions::new()
         .write(true)
         .truncate(true)
         .create(true)
-        .open(GAME_STATE_FILE)?;
+        .open(target)?;
     
     file.write_all(json.as_bytes())?;
     Ok(())
 }
 
 pub fn load_game() -> Result<GameState, Box<dyn std::error::Error>> {
-    let mut file = File::open(GAME_STATE_FILE)?;
+    let target = target_path(GAME_STATE_FILE);
+    let mut file = File::open(target)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let game: GameState = serde_json::from_str(&contents)?;
@@ -73,6 +87,7 @@ pub fn load_game() -> Result<GameState, Box<dyn std::error::Error>> {
 }
 
 pub fn delete_game() -> Result<(), Box<dyn std::error::Error>> {
-    remove_file(GAME_STATE_FILE)?;
+    let target = target_path(GAME_STATE_FILE);
+    remove_file(target)?;
     Ok(())
 }
